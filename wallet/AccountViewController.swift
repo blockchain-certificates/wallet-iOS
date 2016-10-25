4f68f364a609afa49023f6b42c59f3db3235eced
Chris Downie <cdownie@gmail.com>
2016-10-25 16:51:08 -0600

    Adds gravatar support

--- wallet/AccountViewController.swift
+++ wallet/AccountViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import CommonCrypto
 
 class AccountViewController: UIViewController {
 
+    var task : URLSessionDataTask?
+    
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var imageLoadingActivityIndicator: UIActivityIndicatorView!
+    
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var lastNameField: UITextField!
@@ -36,6 +42,31 @@ class AccountViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func emailChanged(_ sender: UITextField) {
+        guard let email = sender.text else {
+            // TODO: Validate that it's actually an email address
+            return
+        }
+        guard let hash = md5(email),
+            let gravatarURL = URL(string: "https://www.gravatar.com/avatar/\(hash)?s=100") else {
+            return
+        }
+        
+        imageLoadingActivityIndicator.isHidden = false
+        imageLoadingActivityIndicator.startAnimating()
+
+        let task : URLSessionDataTask = URLSession.shared.dataTask(with: gravatarURL as URL) { (data, _, _) in
+            if let data = data {
+                self.avatarImageView.image = UIImage(data: data)
+            }
+            
+            self.imageLoadingActivityIndicator.stopAnimating()
+            self.imageLoadingActivityIndicator.isHidden = true
+        }
+        task.resume()
+        self.task = task
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -60,5 +91,24 @@ class AccountViewController: UIViewController {
         UserDefaults.standard.set(firstNameField.text, forKey: UserKeys.firstNameKey)
         UserDefaults.standard.set(lastNameField.text, forKey: UserKeys.lastNameKey)
         UserDefaults.standard.set(emailField.text, forKey: UserKeys.emailKey)
+    }
+    
+    private func md5(_ string: String) -> String? {
+        let length = Int(CC_MD5_DIGEST_LENGTH)
+        var digest = [UInt8](repeating: 0, count: length)
+        
+        
+        if let data = string.data(using: .utf8) {
+            _ = data.withUnsafeBytes {
+                CC_MD5($0, CC_LONG(length), &digest)
+            }
+        }
+        
+        var digestHex = ""
+        for index in 0..<length {
+            digestHex += String(format: "%02x", digest[index])
+        }
+        
+        return digestHex
     }
 }
