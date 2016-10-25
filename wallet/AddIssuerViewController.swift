0d5bdbd4ab015379ebbfd41f3a0d2ac5b445ae59
Chris Downie <cdownie@gmail.com>
2016-10-25 14:15:00 -0600

    Adds issuerURLField to AddIssuerViewController.

--- wallet/AddIssuerViewController.swift
+++ wallet/AddIssuerViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class AddIssuerViewController: UIViewController {
+    @IBOutlet weak var issuerURLField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,26 +17,13 @@ class AddIssuerViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func saveIssuerTapped(_ sender: UIBarButtonItem) {
+        
         dismiss(animated: true, completion: nil)
     }
 
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
