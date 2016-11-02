b7f70e1c2688a9f34c6ef1e4f70ad70f6f2518a0
Chris Downie <cdownie@gmail.com>
2016-11-02 10:58:35 -0600

    Adds scaffolding to proof-of-concept universal links.

--- wallet/AppDelegate.swift
+++ wallet/AppDelegate.swift
@@ -21,6 +21,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.addSubview(JSONLD.shared.webView)
         return true
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
+        let url = userActivity.webpageURL
+        print("is Browsing? \(userActivity.activityType == NSUserActivityTypeBrowsingWeb)")
+        print("url is \(url)")
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
