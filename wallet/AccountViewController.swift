a59e148b461e1db9141e0f9caca27ecf9bb56e47
Chris Downie <cdownie@gmail.com>
2016-11-22 14:46:50 -0700

    Minimum support for allowing the user to set their own passphrase for their wallet.

--- wallet/AccountViewController.swift
+++ wallet/AccountViewController.swift
@@ -43,48 +43,72 @@ class AccountViewController: UIViewController {
     @IBAction func doneTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
-
-    @IBAction func emailChanged(_ sender: UITextField) {
-        guard let email = sender.text else {
-            // TODO: Validate that it's actually an email address
-            return
-        }
-        guard let hash = md5(email),
-            let gravatarURL = URL(string: "https://www.gravatar.com/avatar/\(hash)?s=100") else {
-            return
-        }
-        
-        UserDefaults.standard.set(gravatarURL, forKey: UserKeys.avatarURLKey)
-        
-        loadAvatarImage(from: gravatarURL)
-    }
     
     @IBAction func toggleShowPassphraseTapped(_ sender: UIButton) {
-        if isShowingPassphrase {
-            isShowingPassphrase = !isShowingPassphrase
-            self.toggleShowPassphraseButton.setTitle("Hide passphrase", for: .normal)
-            self.passphraseLabel.text = Keychain.shared.seedPhrase
-        } else if doesUserPassAuthentication() {
-            isShowingPassphrase = !isShowingPassphrase
+        guard !isShowingPassphrase else {
+            isShowingPassphrase = false
             self.toggleShowPassphraseButton.setTitle("Reveal passphrase", for: .normal)
             self.passphraseLabel.text = passphraseExplanation
+            return
+        }
+        
+        authenticateUser { (success, error) in
+            guard success else {
+                // TODO: Have some UI that indicates the failure.
+                return
+            }
+            
+            OperationQueue.main.addOperation {
+                self.isShowingPassphrase = true
+                self.toggleShowPassphraseButton.setTitle("Hide passphrase", for: .normal)
+                self.passphraseLabel.text = Keychain.shared.seedPhrase
+            }
         }
     }
     
     @IBAction func importPassphraseTapped(_ sender: UIButton) {
+        let prompt = UIAlertController(title: nil, message: "What passphrase would you like to import?", preferredStyle: .alert)
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(UIAlertAction(title: "Import", style: .destructive, handler: { (action) in
+            if let passphrase = prompt.textFields?.first?.text,
+                Keychain.isValidPassphrase(passphrase) {
+                // This shouldn't throw if we're for-sure passing it a vaild passphrase
+                try! Keychain.updateShared(with: passphrase)
+                
+                OperationQueue.main.addOperation {
+                    self.isShowingPassphrase = false
+                    self.passphraseLabel.text = "Your passphrase has been updated"
+                    self.toggleShowPassphraseButton.setTitle("Reveal passphrase", for: .normal)
+                }
+            } else {
+                OperationQueue.main.addOperation {
+                    self.isShowingPassphrase = false
+                    self.passphraseLabel.text = "Failed to update your passphrase. Try again?"
+                    self.toggleShowPassphraseButton.setTitle("Reveal passphrase", for: .normal)
+                }
+            }
+
+        }))
+        prompt.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(prompt, animated: true, completion: nil)
     }
 
     
-    func doesUserPassAuthentication() -> Bool {
+    func authenticateUser(completionHandler: @escaping (Bool, Error?) -> Void) {
         let context = LAContext()
         var error : NSError? = nil
         
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            return true
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Authenticate to see your secure passphrase.", reply: completionHandler)
         } else if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
-            return true
+            context.evaluatePolicy(
+                .deviceOwnerAuthentication,
+                localizedReason: "Authenticate to see your secure passphrase.",
+                reply: completionHandler)
         } else {
-            return false
+            // TODO: Use a real error here.
+            completionHandler(false, NSError())
         }
     }
     
@@ -106,57 +130,11 @@ class AccountViewController: UIViewController {
         firstNameField.text = firstName
         lastNameField.text = lastName
         emailField.text = email
-        
-        if let avatarURL = UserDefaults.standard.url(forKey: UserKeys.avatarURLKey) {
-            loadAvatarImage(from: avatarURL)
-        }
-
     }
     
     func saveAccount() {
         UserDefaults.standard.set(firstNameField.text, forKey: UserKeys.firstNameKey)
         UserDefaults.standard.set(lastNameField.text, forKey: UserKeys.lastNameKey)
         UserDefaults.standard.set(emailField.text, forKey: UserKeys.emailKey)
-    }
-    
-    func loadAvatarImage(from url: URL) {
-        OperationQueue.main.addOperation { [weak self] in
-            self?.imageLoadingActivityIndicator.isHidden = false
-            self?.imageLoadingActivityIndicator.startAnimating()
-            
-            let task : URLSessionDataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, _) in
-                OperationQueue.main.addOperation {
-                    if let data = data {
-                        self?.avatarImageView.image = UIImage(data: data)
-                    }
-                    
-                    self?.imageLoadingActivityIndicator.stopAnimating()
-                    self?.imageLoadingActivityIndicator.isHidden = true
-                }
-                self?.task = nil
-            }
-            task.resume()
-            self?.task?.cancel()
-            self?.task = task
-        }
-    }
-    
-    private func md5(_ string: String) -> String? {
-        let length = Int(CC_MD5_DIGEST_LENGTH)
-        var digest = [UInt8](repeating: 0, count: length)
-        
-        
-        if let data = string.data(using: .utf8) {
-            _ = data.withUnsafeBytes {
-                CC_MD5($0, CC_LONG(length), &digest)
-            }
-        }
-        
-        var digestHex = ""
-        for index in 0..<length {
-            digestHex += String(format: "%02x", digest[index])
-        }
-        
-        return digestHex
     }
 }
