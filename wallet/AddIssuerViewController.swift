48482e77043e9f41d067aed0c179d144f568edb2
Chris Downie <cdownie@gmail.com>
2016-12-09 11:48:52 -0700

    Full form layout for add issuer.

--- wallet/AddIssuerViewController.swift
+++ wallet/AddIssuerViewController.swift
@@ -12,18 +12,28 @@ import BlockchainCertificates
 class AddIssuerViewController: UIViewController {
     private var inProgressRequest : CommonRequest?
     var delegate : AddIssuerViewControllerDelegate?
-    var firstName : String!
-    var lastName : String!
-    var email : String!
     
     var identificationURL: URL?
+    var firstName : String?
+    var lastName : String?
+    var emailAddress : String?
     var nonce: String?
     
     @IBOutlet weak var issuerURLField: UITextField!
     
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailAddressField : UITextField!
+    @IBOutlet weak var nonceField : UITextField!
+    
+    
     init(identificationURL: URL? = nil, nonce: String? = nil) {
         self.identificationURL = identificationURL
         self.nonce = nonce
+        firstName = UserDefaults.standard.string(forKey: UserKeys.firstNameKey)
+        lastName = UserDefaults.standard.string(forKey: UserKeys.lastNameKey)
+        emailAddress = UserDefaults.standard.string(forKey: UserKeys.emailKey)
+        
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -43,12 +53,19 @@ class AddIssuerViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [
             NSForegroundColorAttributeName: Colors.tintColor
         ]
-
-        // Do any additional setup after loading the view.
-        fetchAccountData()
+        
+        loadDataIntoFields()
     }
-
-    func saveIssuerTapped(_ sender: UIBarButtonItem) {
+    
+    func loadDataIntoFields() {
+        issuerURLField.text = identificationURL?.absoluteString
+        
+        firstNameField.text = firstName
+        lastNameField.text = lastName
+        emailAddressField.text = emailAddress
+        nonceField.text = nonce
+    }
+    func saveDataIntoFields() {
         guard let urlString = issuerURLField.text else {
             // TODO: Somehow alert/convey the fact that this field is required.
             return
@@ -57,34 +74,37 @@ class AddIssuerViewController: UIViewController {
             // TODO: Somehow alert/convey that this isn't a valid URL
             return
         }
+        identificationURL = url
+        firstName = firstNameField.text
+        lastName = lastNameField.text
+        emailAddress = emailAddressField.text
+        nonce = nonceField.text
+    }
+
+    func saveIssuerTapped(_ sender: UIBarButtonItem) {
+        // TODO: validation.
         
-        identifyAndIntroduceIssuer(at: url)
+        saveDataIntoFields()
+        
+        guard identificationURL != nil,
+            firstName != nil,
+            lastName != nil,
+            emailAddress != nil,
+            nonce != nil else {
+                return
+        }
+        
+        identifyAndIntroduceIssuer(at: identificationURL!)
     }
 
     func cancelTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
     }
     
-    
-    func fetchAccountData() {
-        guard let firstName = UserDefaults.standard.string(forKey: UserKeys.firstNameKey),
-            let lastName = UserDefaults.standard.string(forKey: UserKeys.lastNameKey),
-            let email = UserDefaults.standard.string(forKey: UserKeys.emailKey) else {
-                // TODO: Redirect the user to the Add Account page, since they don't have an account.
-                return
-        }
-        
-        self.firstName = firstName
-        self.lastName = lastName
-        self.email = email
-    }
-    
     func autoSubmitIfPossible() {
-        issuerURLField.text = self.identificationURL?.absoluteString
+        loadDataIntoFields()
         
-        // TODO: Show the nonce
-        
-        let areAllFieldsFilled = firstName != nil && lastName != nil && email != nil && identificationURL != nil
+        let areAllFieldsFilled = firstName != nil && lastName != nil && emailAddress != nil && identificationURL != nil
 
         if areAllFieldsFilled {
             identifyAndIntroduceIssuer(at: identificationURL!)
@@ -92,9 +112,9 @@ class AddIssuerViewController: UIViewController {
     }
     
     func identifyAndIntroduceIssuer(at url: URL) {
-        let targetRecipient = Recipient(givenName: firstName,
-                                        familyName: lastName,
-                                        identity: email,
+        let targetRecipient = Recipient(givenName: firstName!,
+                                        familyName: lastName!,
+                                        identity: emailAddress!,
                                         identityType: "email",
                                         isHashed: false,
                                         publicAddress: Keychain.shared.nextPublicAddress(),
