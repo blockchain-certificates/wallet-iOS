3034941af8c3b3d5cc14277e86586af423442f2b
Chris Downie <cdownie@gmail.com>
2017-01-10 10:29:51 -0700

    Removes generated comments and code from AddIssuerUITests

--- walletUITests/AddIssuerUITests.swift
+++ walletUITests/AddIssuerUITests.swift
@@ -13,21 +13,12 @@ class AddIssuerUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        
         let app = XCUIApplication()
         app.launchArguments = [ "--reset-data" ]
         app.launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
     
     func testAddGameOfThronesIssuer() {
@@ -48,11 +39,9 @@ class AddIssuerUITests: XCTestCase {
         oneTimeCode.typeText("12345")
         
         XCUIApplication().navigationBars["Add Issuer"].buttons["Save"].tap()
-        print(app.collectionViews.cells.count)
         
         XCTAssert(app.navigationBars["Issuers"].exists, "Should be back on the Issuers screen.")
         XCTAssert(app.collectionViews.cells["Game of thrones issuer on testnet"].exists, "Now, we should have a Game of Thrones issuer in the top screen.")
-
     }
     
 }
