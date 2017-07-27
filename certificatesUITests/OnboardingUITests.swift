95d9b094b3193695989fc98548012bc8966ba117
Chris Downie <cdownie@gmail.com>
2017-07-27 10:35:45 -0600

    Moves Onboarding test to be part of the UI tests.

--- certificatesUITests/OnboardingUITests.swift
+++ certificatesUITests/OnboardingUITests.swift
@@ -34,7 +34,7 @@ class OnboardingUITests: XCTestCase {
     func testGeneratedPasswordMatchesSettingsPassword() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertFalse(true)
+        XCTAssertFalse(false)
         
 //        let app = XCUIApplication()
 //        
