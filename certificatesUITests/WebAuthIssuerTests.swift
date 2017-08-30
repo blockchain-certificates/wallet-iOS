240036dee5f137422fc8c14576bed66658939ad6
Chris Downie <cdownie@gmail.com>
2017-08-30 15:16:56 -0600

    Another test, showing that our universal links work for web-auth.

--- certificatesUITests/WebAuthIssuerTests.swift
+++ certificatesUITests/WebAuthIssuerTests.swift
@@ -57,6 +57,39 @@ class WebAuthIssuerTests: XCTestCase {
         XCTAssert(app.collectionViews.cells["Web Auth Issuer"].exists)
     }
     
+    func testAddingWebAuthIssuerFromLink() {
+        let app = XCUIApplication()
+        let safari = XCUIApplication(bundleIdentifier: "com.apple.mobilesafari")
+        
+        // Launch Safari, go to our test page, and click our universal link.
+        safari.launch()
+        safari.otherElements["URL"].tap()
+        safari.textFields["URL"].typeText("http://localhost:1234/links/web-auth-issuer\n")
+        let webpage = safari.staticTexts["Link for Web Auth Issuer"]
+        XCTAssert(webpage.waitForExistence(timeout: 5))
+        safari.links["Click Here"].tap()
+        
+        // Confirm that the deep link launched our app.
+        sleep(10)
+        
+        
+        XCTAssert(app.navigationBars["Log In To Issuer"].exists)
+        
+        let title = app.staticTexts["Web Authentication Challenge"]
+        let exists = NSPredicate(format: "exists == 1")
+        expectation(for: exists, evaluatedWith: title, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        app.links["Yes"].tap()
+        
+        let nav = app.navigationBars["Issuers"]
+        expectation(for: exists, evaluatedWith: nav, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertEqual(app.collectionViews.cells.count, 1)
+        XCTAssert(app.collectionViews.cells["Web Auth Issuer"].exists)
+    }
+    
     func testFailingWebAuth() {
         let app = XCUIApplication()
         XCTAssertEqual(app.collectionViews.cells.count, 0)
