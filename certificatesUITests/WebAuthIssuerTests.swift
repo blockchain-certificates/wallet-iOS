b22822106031fbd2d901403dbee0bbba01e4b568
Chris Downie <cdownie@gmail.com>
2017-08-30 14:49:37 -0600

    Adds a test that shows we handle a failed webauth request properly.

--- certificatesUITests/WebAuthIssuerTests.swift
+++ certificatesUITests/WebAuthIssuerTests.swift
@@ -41,7 +41,7 @@ class WebAuthIssuerTests: XCTestCase {
         app.navigationBars["Add Issuer"].buttons["Save"].tap()
         
         XCTAssert(app.navigationBars["Log In To Issuer"].exists)
-
+        
         let title = app.staticTexts["Web Authentication Challenge"]
         let exists = NSPredicate(format: "exists == 1")
         expectation(for: exists, evaluatedWith: title, handler: nil)
@@ -57,4 +57,39 @@ class WebAuthIssuerTests: XCTestCase {
         XCTAssert(app.collectionViews.cells["Web Auth Issuer"].exists)
     }
     
+    func testFailingWebAuth() {
+        let app = XCUIApplication()
+        XCTAssertEqual(app.collectionViews.cells.count, 0)
+        XCTAssertFalse(app.collectionViews.cells["Web Auth Issuer"].exists)
+        
+        app.collectionViews.buttons["ADD ISSUER"].tap()
+        
+        let elementsQuery = app.scrollViews.otherElements
+        let issuerUrlTextField = elementsQuery.textFields["Issuer URL"]
+        issuerUrlTextField.tap()
+        issuerUrlTextField.typeText("http://localhost:1234/issuer/web-auth")
+        
+        let oneTimeCodeTextField = elementsQuery.textFields["One-Time Code"]
+        oneTimeCodeTextField.tap()
+        oneTimeCodeTextField.tap()
+        oneTimeCodeTextField.typeText("12345")
+        app.navigationBars["Add Issuer"].buttons["Save"].tap()
+        
+        XCTAssert(app.navigationBars["Log In To Issuer"].exists)
+        
+        let title = app.staticTexts["Web Authentication Challenge"]
+        let exists = NSPredicate(format: "exists == 1")
+        expectation(for: exists, evaluatedWith: title, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        app.links["No"].tap()
+        
+        let alert = app.alerts["Add Issuer Failed"]
+        expectation(for: exists, evaluatedWith: alert, handler: nil)
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        alert.buttons["OK"].tap()
+        
+        XCTAssert(app.navigationBars["Add Issuer"].exists)
+    }
 }
