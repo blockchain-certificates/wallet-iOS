a173371bfcb420835ef360d0fed9e40d9f1a37d0
Quinn McHenry <quinn@jqm.us>
2018-02-15 10:30:33 -0500

    Add header cell for issuer detail view [WIP]

---
+++ wallet/IssuerDetailHeaderCell.swift
@@ -0,0 +1,9 @@
+//
+//  IssuerDetailHeaderCell.swift
+//  certificates
+//
+//  Created by Quinn McHenry on 2/14/18.
+//  Copyright © 2018 Learning Machine, Inc. All rights reserved.
+//
+
+import Foundation
